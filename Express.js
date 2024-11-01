e3db883d95edf3d8f7bd353265b6426c895cc494
oseembiya <ozeembiya@gmail.com>
2024-11-01 14:53:44 +0000

    Updating express.js

--- Express.js
+++ Express.js
@@ -1,3 +1,15 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
+const app = express();
+
 // middleware
+
+app.use(cors());
+
 // routes
+
+// start the server
+const PORT = 8000;
+app.listen(PORT, function () {
+  console.log(`Server is running on https://localhost:${PORT}`);
+});
